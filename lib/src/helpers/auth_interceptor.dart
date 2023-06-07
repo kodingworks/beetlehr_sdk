1ad60dffd7793ef68db94b8a036782c5d6d279c8
donisaputradev <donisaputradev@gmail.com>
2023-06-07 07:48:29 +0700

    ✨feature: authentication

--- lib/src/helpers/auth_interceptor.dart
+++ lib/src/helpers/auth_interceptor.dart
@@ -1,17 +1,16 @@
-import 'package:beetlehr_sdk/src/exceptions/exceptions.dart';
+import 'dart:io';
+
+import 'package:beetlehr_sdk/src/shared_preferences/shared_preferences.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:dio/dio.dart';
 
 /// Error handling when error in interceptor about authentication
 class AuthHttpInterceptor extends InterceptorsWrapper {
   /// Repository to get data current token
-  final String token;
-  final String deviceId;
   final String language;
 
   ///
   AuthHttpInterceptor({
-    required this.deviceId,
-    required this.token,
     required this.language,
   });
 
@@ -20,28 +19,29 @@ class AuthHttpInterceptor extends InterceptorsWrapper {
       RequestOptions options, RequestInterceptorHandler handler) async {
     final optionHeaders = <String, Object>{};
 
+    final token = await getToken();
+
     if (options.headers['unAuthorize'] != true) {
       optionHeaders.putIfAbsent('Authorization', () => 'Bearer $token');
     }
-    optionHeaders.putIfAbsent('user-device', () => deviceId);
+
+    late String? deviceId;
+
+    final deviceInfo = DeviceInfoPlugin();
+
+    if (Platform.isAndroid) {
+      final androidInfo = await deviceInfo.androidInfo;
+      deviceId = androidInfo.id;
+    } else if (Platform.isIOS) {
+      final iosInfo = await deviceInfo.iosInfo;
+      deviceId = iosInfo.identifierForVendor;
+    }
+
+    optionHeaders.putIfAbsent('user-device', () => deviceId!);
 
     optionHeaders.putIfAbsent('Accept-Language', () => language);
 
     options.headers.addAll(optionHeaders);
     handler.next(options);
-  }
-
-  @override
-  Future onError(DioError err, ErrorInterceptorHandler handler) async {
-    final isError403 = (err.response?.statusCode == 403 &&
-        err.requestOptions.headers['ignore_403'] != true);
-
-    final isError401 = (err.response?.statusCode == 401 &&
-        err.requestOptions.headers['ignore_401'] != true);
-
-    if (isError401 || isError403) {
-      throw UnAuthenticationServerException(message: 'Unauhentication');
-    }
-    handler.next(err);
   }
 }
